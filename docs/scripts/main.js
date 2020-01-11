7a19a1c2b7216d40400938d474f5ea6ac20c87da
Doug Rosman <drosman@saic.edu>
2020-01-10 18:23:54 -0600

    upload site

--- docs/scripts/main.js
+++ docs/scripts/main.js
@@ -30,7 +30,7 @@ for(let i = 0; i < yolo.length; i++) {
     e.onmouseenter = function(){
         let randomHue = hueValues[Math.floor(Math.random()*hueValues.length)];
         
-        e.style.border = `2px solid hsla(${randomHue}, 100%, 60%, 1)`;
+        e.style.border = `3px solid hsla(${randomHue}, 100%, 60%, 1)`;
         currentLabel.style.backgroundColor = `hsla(${randomHue}, 100%, 60%, 1)`;
         
         currentLabel.style.color = "black";
@@ -41,48 +41,6 @@ for(let i = 0; i < yolo.length; i++) {
     e.onmouseleave = function() {
         currentLabel.style.color = "hsla(200, 100%, 60%, 0)";
         currentLabel.style.backgroundColor = `hsla(0, 100%, 60%, 0)`;
-        e.style.border = `2px solid hsla(0, 100%, 60%, 0)`;
+        e.style.border = `3px solid hsla(0, 100%, 60%, 0)`;
     }
 }
-
-
-
-
-
-// for(let i = 0; i < yolo.length; i++) {
-//     let e = yolo[i];
-//     e.onmouseenter = function(){
-//         counter++;
-        
-//         let randomObj = objects[Math.floor(Math.random()*objects.length)];
-        
-//         label = document.createElement("h6");
-//         label.innerHTML = randomObj;
-//         //labelNode = document.createTextNode(randomObj);
-//         //console.log(labelNode);
-//         //label.appendChild(labelNode);
-//         // e.insertBefore(label, e.childNodes[0]);
-//         e.insertAdjacentElement("beforebegin", label);
-
-//         label.style.position = "absolute";
-//         label.style.top = "-2px";
-
-//         e.style.border = "2px solid blue";     
-//         console.log(e);
-//     }
-
-//     if(counter > 1) {
-//         console.log("counter");
-//         e.onmouseleave = function() {
-//             e.style.border = "2px solid white";
-//             e.parentElement.removeChild(e.parentElement.childNodes[counter+1]);
-//             counter--;
-//         } 
-//     } else {
-//         e.onmouseleave = function() {
-//             e.style.border = "2px solid white";
-//             e.parentElement.removeChild(e.parentElement.childNodes[0]);
-//             counter--;
-//         }
-//     }
-// }
