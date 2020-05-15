e8d94ee61bfd390d38545cfdbe8869e9c78809fb
Doug Rosman <drosman@saic.edu>
2020-05-15 10:49:12 -0500

    sure

--- docs/final/avery/sketch.js
+++ docs/final/avery/sketch.js
@@ -1,15 +1,14 @@
 let loop = true;
-let capture;
-
-let w = 1280;
-let h = 720;
-
 let threshold = 80;
 let threshold01 = 160;
 let threshold02 = 240;
+let capture;
+let w = 1280;
+let h = 720;
 
 function setup() {
-  createCanvas(w, h);
+  let canvas = createCanvas(w, h);
+  canvas.parent("sketch");
   let constraints = {audio:false,video:{width:{min:320,ideal:w,max:1920},height:{min:240,ideal:h,max:1080},frameRate: {min: 1.0, max: 60.0}}};
   capture = createCapture(constraints);
   capture.hide();
@@ -17,43 +16,47 @@ function setup() {
 }
 
 function draw() {
-  capture.loadPixels();
-  threshold = map(sin(frameCount/10),-1,1,1,255);
+  if(loop){
+    capture.loadPixels();
+    threshold = map(sin(frameCount/10),-1,1,1,255);
 
-  for (var y = 0; y < h; y++ ) {
-    for (var x = 0; x < w; x++ ) {
-      let index = (x + y*w)*4;
-      let r = capture.pixels[index+0];
-      let g = capture.pixels[index+1];
-      let b = capture.pixels[index+2];
-      let totalBrightness = r + g + b;
-      let brightness = totalBrightness/3.0;
-      
-      if(brightness < threshold) {
-        //set pixels to black
-        capture.pixels[index+0] = 0;
-        capture.pixels[index+1] = 0;
-        capture.pixels[index+2] = 0;
-      } else if(brightness > threshold01 && brightness < threshold02) {
-        //set pixels to color
-        capture.pixels[index+0] = 255;
-        capture.pixels[index+1] = 0;
-        capture.pixels[index+2] = 255;
-      } else {
-        //set pixels to white
-        capture.pixels[index+0] = 255;
-        capture.pixels[index+1] = x;
-        capture.pixels[index+2] = y;
+    for (var y = 0; y < h; y++ ) {
+      for (var x = 0; x < w; x++ ) {
+        let index = (x + y*w)*4;
+        let r = capture.pixels[index+0];
+        let g = capture.pixels[index+1];
+        let b = capture.pixels[index+2];
+        let totalBrightness = r + g + b;
+        let brightness = totalBrightness/3.0;
+        
+        if(brightness < threshold) {
+          //set pixels to black
+          capture.pixels[index+0] = 0;
+          capture.pixels[index+1] = 0;
+          capture.pixels[index+2] = 0;
+        } else if(brightness > threshold01 && brightness < threshold02) {
+          //set pixels to color
+          capture.pixels[index+0] = 255;
+          capture.pixels[index+1] = 0;
+          capture.pixels[index+2] = 255;
+        } else {
+          //set pixels to white
+          capture.pixels[index+0] = 255;
+          capture.pixels[index+1] = x;
+          capture.pixels[index+2] = y;
+        }
       }
     }
+    capture.updatePixels();
+    
+    push();
+      translate(w, 0);
+      scale(-1, 1);
+      image(capture, 0, 0);
+    pop();
+  } else {
+    // do nothing
   }
-  capture.updatePixels();
-  
-  push();
-    translate(w, 0);
-    scale(-1, 1);
-    image(capture, 0, 0);
-  pop();
 }
 
 function windowResized() {
