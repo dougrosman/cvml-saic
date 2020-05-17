f08ca5cd550a7cc7671cd14f1d21dbe8c860f594
Doug Rosman <drosman@saic.edu>
2020-05-17 17:35:57 -0500

    add artist-page template stuff

--- docs/final/scripts/gallery-utils.js
+++ docs/final/scripts/gallery-utils.js
@@ -4,26 +4,32 @@ $(document).ready(function(){
 
     if(typeof(window.orientation)=="undefined") {
         $("img").click(toggleImageModal);
-        $(".modal-background").click(toggleBackgroundModal);
-    }
+        $(".modal-background").click(toggleModalOff);
 
+        $("body").keydown("esc", function() {
+            if(event.which == 27 && active){
+                toggleModalOff();
+            }
+        });
+        $(window).scroll(function(){
+            if(active){
+                toggleModalOff();
+            }
+        })
+    }
     function toggleImageModal(){
         if(!active){
             $(this).css("position", "fixed").css("z-index", "6").css("transform", "translate(-50%, -50%) scale(1.3)");
             $(".modal-background").toggle();
             active = !active;
         } else {
-            $("img").css("position", "static").css("z-index", "0").css("transform", "translate(0,0) scale(1)");
-            $(".modal-background").toggle();
-            active = !active;
+            toggleModalOff();
         }
     }
-
-
-    function toggleBackgroundModal(){
+    function toggleModalOff(){
         $("img").css("position", "static").css("z-index", "0").css("transform", "translate(0,0) scale(1)");
-            $(".modal-background").toggle();
-            active = !active;
+        $(".modal-background").toggle();
+        active = !active;
     }
 
 })
