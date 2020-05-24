0cab9cf184bad0f7b19d0836e2c8e91aed370c45
Doug Rosman <drosman@saic.edu>
2020-05-24 11:08:48 -0500

    add blake page

--- docs/final/scripts/gallery-utils.js
+++ docs/final/scripts/gallery-utils.js
@@ -20,7 +20,7 @@ $(document).ready(function(){
     function toggleImageModal(){
         if(!active){
             let ratio = scaleImage($(this));
-            console.log(ratio);
+            // console.log(ratio);
             $(this).css("position", "fixed").css("z-index", "6").css("transform", `translate(-50%, -50%) scale(${ratio})`);
             $(".modal-background").toggle();
             active = !active;
@@ -37,16 +37,20 @@ $(document).ready(function(){
 
 
     function scaleImage(image) {
-        let w = image[0].width;
-        let h = image[0].height;
-        let _ratio;
+        // let w = image[0].width;
+        // let h = image[0].height;
+        let w = 512;
+        let h = 512;
+
+        console.log("width: " + w);
+        console.log("height: " + h);
+        let _ratio = 1;
         if(w > h) {
-            _ratio = window.innerWidth/w;
-        } else if(h < w) {
             _ratio = window.innerHeight/h;
+        } else if(h < w) {
+            _ratio = window.innerWidth/w;
         } else {
             if(window.innerHeight > window.innerWidth) {
-                
                 _ratio = window.innerWidth/w;
             } else {
                 _ratio = window.innerHeight/h;
